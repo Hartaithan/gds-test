9279fa776d3f4b7e11509756da814abea9d35b84
hartaithan <hartaithan@gmail.com>
2022-04-01 16:39:44 +0900

    add empty todos message

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="list-view">
+  <div class="list-view" v-if="todos.length > 0">
     <ListItem v-for="todo in todos" :key="todo.id" :todo="todo" />
   </div>
+  <p class="list-view__empty" v-else>Tasks have not yet been added</p>
 </template>
 
 <script setup lang="ts">
@@ -14,4 +15,11 @@ const todos = computed(() => store.state.todos)
 </script>
 
 <style scoped lang="scss">
+.list-view {
+  &__empty {
+    width: 100%;
+    text-align: center;
+    margin-top: 20px;
+  }
+}
 </style>
