4996d1afbd2ff1770b09a5c1d167d1dd3957a48a
hartaithan <hartaithan@gmail.com>
2022-04-01 14:20:07 +0900

    setup routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,16 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import ListView from '../views/ListView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'ListView',
+    component: ListView
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/cards',
+    name: 'CardsView',
+    component: () => import('../views/CardsView.vue')
   }
 ]
 
---
+++ src/views/CardsView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="cards-view">Cards view</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+</style>
